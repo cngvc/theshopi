1ec58fdcfcc2cd30386d503e07329f8b8d0857e5
cg <congvc98@gmail.com>
2025-02-28 22:45:09 +0700

    add refresh token logic

---
+++ servers/auth-service/src/controllers/refresh-token.controller.ts
@@ -0,0 +1,20 @@
+import { authService } from '@auth/services/auth.service';
+import { BadRequestError } from '@cngvc/shopi-shared';
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+class RefreshTokenController {
+  async refreshToken(req: Request, res: Response): Promise<void> {
+    const existingUser = await authService.getAuthUserById(req.currentUser!.id);
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials', 'refreshToken() method error');
+    }
+    const userJWT = authService.signToken(existingUser.id!, existingUser.email!, existingUser.username!);
+    if (!userJWT) {
+      throw new BadRequestError('Error when signing token', 'refreshToken() method error');
+    }
+    res.status(StatusCodes.OK).json({ message: 'Refresh token successfully', token: userJWT });
+  }
+}
+
+export const refreshTokenController = new RefreshTokenController();
--- servers/auth-service/src/routes.ts
+++ servers/auth-service/src/routes.ts
@@ -1,15 +1,17 @@
 import { BASE_PATH } from '@auth/constants/path';
 import { authRoutes } from '@auth/routes/auth.route';
+import { currentUserRoutes } from '@auth/routes/current-user.route';
 import { healthRoutes } from '@auth/routes/health.route';
 import { seedRoutes } from '@auth/routes/seed.route';
+import { tokenRoutes } from '@auth/routes/token.route';
 import { Application } from 'express';
-import { currentUserRoutes } from './routes/current-user.route';
 
 const appRoutes = (app: Application): void => {
   app.use(BASE_PATH, healthRoutes.routes());
   app.use(BASE_PATH, seedRoutes.routes());
   app.use(BASE_PATH, authRoutes.routes());
   app.use(BASE_PATH, currentUserRoutes.routes());
+  app.use(BASE_PATH, tokenRoutes.routes());
 };
 
 export { appRoutes };
